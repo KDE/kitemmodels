b41555aa1636161e2d7798d23b32307249193306
Friedrich W. H. Kossebau <kossebau@kde.org>
2019-11-18 09:53:51 +0100

    API dox: fixes for KNumberModel docs
    * note CamelCase include for class
    * use @li for list items, @c for code & variable values
    * move property docs to actual properties, not setters
    * ensure /** for begin of dpxygen comment
    * use current property names over informal alias names
      (max, maximum -> maximumValue)
    
    GIT_SILENT

--- src/core/knumbermodel.h
+++ src/core/knumbermodel.h
@@ -29,20 +29,48 @@
 class KNumberModelPrivate;
 
 /**
+ * @class KNumberModel knumbermodel.h KNumberModel
+ *
  * Creates a model of entries from N to M with rows at a given interval
  *
- * The model contains two Roles
- * display, the number represented as a string
- * value, the actual value as a number
+ * The model contains two roles:
+ * @li display - the number represented as a string
+ * @li value - the actual value as a number
  *
  * @since 5.65
  */
 class KITEMMODELS_EXPORT KNumberModel : public QAbstractListModel
 {
     Q_OBJECT
+
+    /**
+     * The minimum value for the model
+     *
+     * The default value is @c 1.0.
+     */
     Q_PROPERTY (qreal minimumValue READ minimumValue WRITE setMinimumValue NOTIFY minimumValueChanged)
+    /**
+     * The maximum value for the model
+     *
+     * The default value is @c 1.0.
+     *
+     * @note  If @c maximumValue is a multiple of @c stepSize added to @c minimumValue
+     * it will be included. Otherwise it will not be reached.
+     * E.g. in a model with a @c minimumValue of 0.0, a @c maximumValue of 1.0 and a @c stepSize of 0.3, the final row will be 0.9.
+     */
     Q_PROPERTY (qreal maximumValue READ maximumValue WRITE setMaximumValue NOTIFY maximumValueChanged)
+    /**
+     * Step between listed entries
+     *
+     * The default value is @c 1.0.
+     */
     Q_PROPERTY (qreal stepSize READ stepSize WRITE setStepSize NOTIFY stepSizeChanged)
+    /**
+     * Defines the string representation of the number,
+     * e.g. "1,000" or "1000".
+     *
+     * Default is @c QLocale::Default.
+     */
     Q_PROPERTY (QLocale::NumberOptions formattingOptions READ formattingOptions WRITE setFormattingOptions NOTIFY formattingOptionsChanged)
 
 public:
@@ -54,36 +82,15 @@ public:
         ValueRole = Qt::UserRole
     };
 
-    /**
-     * The minimum value for the model.
-     * The default value is 1.
-     */
     void setMinimumValue(qreal minimumValue);
     qreal minimumValue() const;
 
-    /**
-     * The maximum value in this model.
-     * The default value is 1.
-     *
-     * @note  If max is a multiple of @arg step it will be included. Otherwise it will not be reached
-     * i.e in a model with a min of 0 with a maximum of 1 with a step size of 0.3, the final row will be 0.9.
-     *
-     */
     void setMaximumValue(qreal maximumValue);
     qreal maximumValue() const;
 
-    /*
-     * Step between listed entries
-     * The default value is 1.0
-     */
     void setStepSize(qreal stepSize);
     qreal stepSize() const;
 
-    /**
-     * Define how the string representation of the number should be presented
-     * i.e "1,000" or "1000"
-     * Default is QLocale::Default
-     */
     void setFormattingOptions(QLocale::NumberOptions options);
     QLocale::NumberOptions formattingOptions() const;
 
